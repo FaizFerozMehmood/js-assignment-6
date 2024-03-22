66e2f286c052cd6965816bad00ded575baa04a94
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-03-22 06:17:28 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+//• Write a loop to print numbers from 1 to 10.
+document.write("<h1>Printing numbers from 1 to 10</h1>")
+for(var i = 1; i<=10;i++){
+    document.write(i + "<br>")
+}
+// Write a loop to print even numbers from 1 to 20.
+document.write("<h1>Even number using loop</h1>")
+for (var i  = 2; i <=20;i +=2 ){
+    document.write(i+"<br>")
+}
+
+//• Write a loop to print odd numbers from 1 to 15.
+document.write("<h1>ODD number using loop</h1>")
+for (var i = 1; i < 20 ;i+=2){
+    document.write(i +"<br>")
+}
+
+//• Write a loop to calculate and print the factorial of a number (let's say 5).
+for (var i = 0; i <= 30;i ++){
+
+}
+//Write a loop to print the multiplication table of a number (let's say 7) up to 10
+document.write("<h1>Multiplication table</h1>")
+var number = 7;
+for (var i = 1; i<=10; i ++){
+    var result = number * i;
+    document.write (`${number} *${i} =${result}`+"<br>")
+    
+}
+//Write a loop to print the reverse of a string (let's say "hello").
+
+
+
+
+
+// Write a loop to print the square of numbers from 1 to 10.
+document.write("<h1>Square of numbers from 1 to 10</h1>");
+
+for (i = 1; i<=10; i++){
+    document.write(i * i+" ")
+}
+//• Write a loop to find and print the smallest element in an array (let's say [3, 7, 2, 9, 5]).
+
+
