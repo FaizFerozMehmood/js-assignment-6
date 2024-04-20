8f79ee8142a4baab5fdee78934746672056fed61
FaizFerozMehmood <ferozfaaiz5@gmail.com>
2024-04-20 10:17:32 +0500

    3rd-commit

--- practice/app.js
+++ practice/app.js
@@ -97,6 +97,112 @@
 // var num = "hello world!";
 // var num2 = num.repeat(2);
 // console.log(num2)
-var userName = " mr faiz mahmood kurovi";
-var userRes = userName.replaceAll("FAIz","fEROZ")
-console.log(userRes)
+// var userName = " mr faiz mahmood kurovi";
+// var userRes = userName.replaceAll("FAIz","fEROZ")
+// console.log(userRes)
+
+// / var userInfo = prompt('Add something')
+
+// var vowelCount = 0
+// var nonVowelCount = 0
+// for (var i = 0; i < userInfo.length; i++) {
+//     console.log(userInfo.charAt(i))
+//     var alphabet = userInfo.charAt(i).toLowerCase()
+//     if (alphabet == 'a' || alphabet == 'i' || alphabet == 'e' || alphabet == 'o' || alphabet == 'u') {
+//         vowelCount++
+//     } else if (alphabet !== ' ') {
+//         nonVowelCount++
+//     }
+// } console.log('Vowels are ', vowelCount, 'Non Vowels are', nonVowelCount)
+
+
+// var userInput= prompt("Enter words or sentences to count vowel and non")
+// var vowelCount = 0;
+// var nonvowelCount = 0;
+// for(var i =0;i<userInput.length; i++){
+//     console.log(userInput.charAt(i))
+//     var alphabet =userInput.charAt(i).toLowerCase()
+//     if(alphabet=="a"|| alphabet=="e"|| alphabet=="i"|| alphabet=="o"|| alphabet=="u"){
+//         vowelCount++
+//     }
+// else if (alphabet!==" "){
+//     nonvowelCount++
+
+// }
+
+// }console.log("vowel",vowelCount,"nonvowel",nonvowelCount)
+
+// var userInput= prompt("enter here");
+// var vowelCount= 0;
+// var nonVowelCount = 0;
+// for(var i =0; i<userInput.length; i++){
+//     console.log(userInput.charAt(i))
+//     var alphabet=userInput.charAt(i).toLowerCase();
+//     if(alphabet =="a"||alphabet=="e"||alphabet =="i"||alphabet=="o"||alphabet=="u"){
+//         vowelCount++
+//     }
+//     else if (alphabet!==" "){
+//         nonVowelCount++
+//     }
+
+// }
+// console.log("vowels from usrinput are",vowelCount,"Non vowels found in userINPUT ARE",nonVowelCount )
+// var userInput =prompt("enter number to check whether the is even or odd")
+// var even = 0
+// var odd = 0
+// var num =parseInt( userInput)
+// for (var i =0; i<=num.length;i++){
+//     if (num % 2 ===0){
+//         even++
+//     }
+//     else if(num % 2===1) {
+//         odd++
+//     }
+    
+// }
+// console.log("even",even,"odd" ,odd) 
+
+
+// var userInput= prompt("enter number")
+// var evenNumber = 0;
+// var oddNumber = 0 ;
+// if(!isNaN(userInput)){
+//     for (var i = 0; i <userInput.length;i++){
+//         var digits = parseInt(userInput[i])
+//             if(digits %2===0){
+//              evenNumber++
+//             }else{
+//                 oddNumber++
+//             }
+//         }
+//     }
+// console.log("evenNUmber",evenNumber,"oddNumber",oddNumber)
+
+// var userInput = prompt("ENTER WORDS OR ANY NAMES")
+
+// var vowels = 0;
+// var nonvowel = 0;
+// for(var i =0; i < userInput.length; i++){
+//     console.log(userInput.charAt(i))
+//     var alphabet=userInput.charAt(i).toLowerCase()
+//     if (alphabet=="a"||alphabet=="e"||alphabet=="i"||alphabet=="o"||alphabet=="u"){
+//      vowels++
+//     }else if(alphabet!==" "){
+
+//         nonvowel++
+//     }
+// }
+// console.log("vowels",vowels,"nonVowels",nonvowel)
+var userInfo= prompt('fill it out plz')
+var vowel= 0
+var nonvowel = 0
+for(var i = 0; i<userInfo.length; i++){
+var alphabet= userInfo.charAt(i).toLowerCase()
+if(alphabet==="a"||alphabet==="e"||alphabet==="i"||alphabet==="o"||alphabet==="u"){
+    vowel++
+
+}else if(alphabet !==' '){
+    nonvowel++
+}
+}
+console.log("vowel",vowel,"nonvowel",nonvowel)
